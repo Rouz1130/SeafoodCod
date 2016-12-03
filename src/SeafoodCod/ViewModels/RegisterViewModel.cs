fb78fa5cade55c5d4a5734ed625ff9ad744b49df
rouz majlessi <rmajlessi@gmail.com>
2016-12-02 19:51:17 -0800

    creage viewmodels folder also add login and register files and properties

---
+++ src/SeafoodCod/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeafoodCod.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name="Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name="Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name="Confirm Password")]
+        [Compare("Password", ErrorMessage= "Please try again password does not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
