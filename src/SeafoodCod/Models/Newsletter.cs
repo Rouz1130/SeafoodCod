3adb057e37b3305e9583be70c609610d1fef4954
rouz majlessi <rmajlessi@gmail.com>
2016-12-03 18:50:11 -0800

    add newsletter model

---
+++ src/SeafoodCod/Models/Newsletter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeafoodCod.Models
+{
+    [Table("newsletters")]
+    public class Newsletter
+    {
+        [Key]
+        public int NewsletterId { get; set; }
+        public string Email { get; set; }
+        public string Country { get; set}
+    }
+}
